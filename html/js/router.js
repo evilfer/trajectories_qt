8349779a356b04eef29a8bb1f1db489fa6d80f65
Eloy Villasclaras <evilfer@gmail.com>
2013-07-18 16:51:03 +0100

    Added sim menu.

--- html/js/router.js
+++ html/js/router.js
@@ -2,11 +2,27 @@
 App.Router.map(function() {
   this.resource('index', {path: '/'});
   this.resource('simulations');
-  this.resource('simulation', {path: 'simulation/:simulationMetadata_id'});
+  this.resource('simulation', {path: 'simulation/:simulation_id'}, function() {
+    this.route('config');
+    this.route('ship');
+    this.route('mission');
+  });
 });
 
 App.SimulationsRoute = Ember.Route.extend({
   model: function() {
-    return App.SimulationMetadata.find();
+    return App.Simulation.find();
+  }
+});
+
+App.SimulationRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.Simulation.find(params.simulation_id);
+  }
+});
+
+App.SimulationIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('simulation').get('metadata');
   }
 });
