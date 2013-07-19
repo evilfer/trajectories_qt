a22ab664b2571227968fe6dc96b3aecc30c8dc63
Eloy Villasclaras <evilfer@gmail.com>
2013-07-19 15:46:00 +0100

    New format for metadata form.

--- html/js/controllers/simulation_index_controller.js
+++ html/js/controllers/simulation_index_controller.js
@@ -1,18 +1,27 @@
 
 App.SimulationIndexController = Ember.ObjectController.extend({
   editingAttr: false,
-  initialValue: false,
   editingTitle: function() {
     return this.editingAttr === 'title';
   }.property('editingAttr'),
+  editingDescription: function() {
+    return this.editingAttr === 'description';
+  }.property('editingAttr'),
   editTitle: function() {
-    this.set('initialValue', this.get('title'));
     this.set('editingAttr', 'title');
   },
-  acceptChanges: function() {
-    this.set('editingAttr', false);
-    this.get('model').save();
-  }
+  editDescription: function() {
+    this.set('editingAttr', 'description');
+  },
+  acceptChanges: function(field) {
+    if (field === this.get('editingAttr')) {
+      this.set('editingAttr', false);
+      this.get('model').save();
+    }
+  },
+  formattedDescription: function() {
+    return this.get('description').replace(/\n\r?/g, '<br />');
+  }.property('description').cacheable()
 });
 
 
--- html/js/views/edit_simulation_metadata_view.js
+++ html/js/views/edit_simulation_metadata_view.js
@@ -1,16 +1,27 @@
 
 App.EditSimulationMetadataTitleView = Ember.TextField.extend({
   classNames: ['edit'],
-
-  insertNewline: function () {
-    this.get('controller').acceptChanges();
+  size: 30,
+  insertNewline: function() {
+    this.get('controller').acceptChanges('title');
   },
-
-  focusOut: function () {
-    this.get('controller').acceptChanges();
+  focusOut: function() {
+    this.get('controller').acceptChanges('title');
   },
+  didInsertElement: function() {
+    this.$().focus();
+  }
+});
 
-  didInsertElement: function () {
+
+App.EditSimulationMetadataDescriptionView = Ember.TextArea.extend({
+  classNames: ['edit'],
+  rows: 15,
+  cols: 30,
+  focusOut: function() {
+    this.get('controller').acceptChanges('description');
+  },
+  didInsertElement: function() {
     this.$().focus();
   }
 });
