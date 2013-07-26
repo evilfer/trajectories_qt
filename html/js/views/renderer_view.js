bb22b43fbbe691da254f124996faa64efbc871aa
Eloy Villasclaras <evilfer@gmail.com>
2013-07-26 09:58:05 +0100

    Canvas resize working.

--- html/js/views/renderer_view.js
+++ html/js/views/renderer_view.js
@@ -1,71 +1,57 @@
 
 App.RendererView = Ember.View.extend({
   classNames: ['t-canvas'],
+  willDestroyElement: function() {
+  },
   didInsertElement: function() {
-    // set the scene size
-    var WIDTH = 400,
-            HEIGHT = 300;
-
-// set some camera attributes
-    var VIEW_ANGLE = 45,
-            ASPECT = WIDTH / HEIGHT,
-            NEAR = 0.1,
-            FAR = 10000;
-
-// get the DOM element to attach to
-// - assume we've got jQuery to hand
     var $container = $('.t-canvas');
 
-// create a WebGL renderer, camera
-// and a scene
+    var viewAngle = 45,
+            near = 0.1,
+            far = 10000;
+
     var renderer = new THREE.WebGLRenderer({clearColor: 0x202020, clearAlpha: 1});
-    var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-
+    var camera = new THREE.PerspectiveCamera(viewAngle, 1, near, far);
     var scene = new THREE.Scene();
-
-// add the camera to the scene
     scene.add(camera);
 
-// the camera starts at 0,0,0
-// so pull it back
     camera.position.z = 300;
 
-// start the renderer
-    renderer.setSize(WIDTH, HEIGHT);
-
-// attach the render-supplied DOM element
     $container.append(renderer.domElement);
+    var $canvas = $container.find('canvas');
 
     var radius = 50,
-            segments = 16,
-            rings = 16;
+            segments = 32,
+            rings = 32;
 
-    var sphereMaterial =
-            new THREE.MeshLambertMaterial(
-            {
-              color: 0xCCFF00
-            });
-    var sphere = new THREE.Mesh(
-            new THREE.SphereGeometry(
-            radius,
-            segments,
-            rings),
-            sphereMaterial);
+    var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xCCFF00});
+    var sphere = new THREE.Mesh(new THREE.SphereGeometry(radius, segments, rings), sphereMaterial);
 
-// add the sphere to the scene
     scene.add(sphere);
 
-    var pointLight =
-            new THREE.PointLight(0xFFFFFF);
+    var pointLight = new THREE.PointLight(0xFFFFFF);
 
-// set its position
     pointLight.position.x = 10;
     pointLight.position.y = 50;
     pointLight.position.z = 130;
 
-// add to the scene
     scene.add(pointLight);
 
-    renderer.render(scene, camera);
+    var resizeF = function() {
+      var width = $container.width();
+      var height = $container.height();
+
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+    };
+    
+    $container.resize(resizeF);
+    resizeF();
+
+    (function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    })();
   }
 });
