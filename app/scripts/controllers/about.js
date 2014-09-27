fd4974c32a5b1b5b3617ef04b59c5da7b2b11b39
Eloy Villasclaras <evilfer@gmail.com>
2014-09-27 21:19:01 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trajectoriesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trajectoriesApp
+ */
+angular.module('trajectoriesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trajectoriesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trajectoriesApp
+ */
+angular.module('trajectoriesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
