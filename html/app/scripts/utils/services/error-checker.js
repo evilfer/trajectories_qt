844cf0097193f178f19144f948125410e7a0c270
evilfer <evilfer@gmail.com>
2014-09-29 23:14:18 +0100

    HTML
    First working version of utils:service:ErrorChecker

--- html/app/scripts/utils/services/error-checker.js
+++ html/app/scripts/utils/services/error-checker.js
@@ -12,11 +12,17 @@ angular.module('utils').factory('ErrorCheckerService', function () {
 
   var ErrorCheckerService = function (dataStructure) {
     this.dataStructure = dataStructure;
+    this._methods = {
+      'obj': '_checkObj',
+      'number': '_checkNumber',
+      'string': '_checkString',
+      'array': '_checkArray'
+    };
   };
 
   ErrorCheckerService.prototype.check = function (data) {
     var errors = {};
-    this._checkNode(errors, 'ROOT', data, '_ROOT_');
+    this._checkNode(errors, 'root', data, '_ROOT_');
     return errors;
   };
 
@@ -27,12 +33,16 @@ angular.module('utils').factory('ErrorCheckerService', function () {
     errors[dataPath].push(error);
   };
 
+  ErrorCheckerService.prototype._appendPath = function (base, path) {
+    return base + '.' + path;
+  };
+
   ErrorCheckerService.prototype._checkNode = function (errors, dataPath, data, type) {
     var def = this.dataStructure[type];
-    switch (def[0]) {
-      case 'obj':
-        this._checkObj(errors, dataPath, data, def[1]);
-    }
+
+    var typeName = def[0];
+    var typeOptions = def.length > 1 ? def[1] : {};
+    this[this._methods[typeName]](errors, dataPath, data, typeOptions);
   };
 
   ErrorCheckerService.prototype._checkObj = function (errors, dataPath, data, objDef) {
@@ -40,10 +50,60 @@ angular.module('utils').factory('ErrorCheckerService', function () {
       this._addError(errors, dataPath, 'type:obj');
     } else {
       for (var key in objDef) {
-        console.log(key, objDef[key]);
         if (objDef.hasOwnProperty(key)) {
           if (objDef[key][0] && !(data[key])) {
-            this._addError(errors, dataPath + '.' + key, 'missing');
+            this._addError(errors, this._appendPath(dataPath, key), 'missing');
+          }
+        }
+      }
+
+      for (key in data) {
+        if (data.hasOwnProperty(key)) {
+          if (key in objDef) {
+            var typeName = objDef[key][1];
+            this._checkNode(errors, this._appendPath(dataPath, key), data[key], typeName);
+          } else {
+            this._addError(errors, this._appendPath(dataPath, key), 'notallowed');
+          }
+        }
+      }
+    }
+  };
+
+  ErrorCheckerService.prototype._checkNumber = function (errors, dataPath, data, objDef) {
+    if ((typeof data) != 'number') {
+      this._addError(errors, dataPath, 'type:number');
+    }
+  };
+
+  ErrorCheckerService.prototype._checkString = function (errors, dataPath, data, objDef) {
+    if ((typeof data) != 'string') {
+      this._addError(errors, dataPath, 'type:string');
+    }
+  };
+
+  ErrorCheckerService.prototype._checkArray = function (errors, dataPath, data, objDef) {
+    if (!data || (typeof data) != 'object' || !(data instanceof Array)) {
+      this._addError(errors, dataPath, 'type:array');
+    } else {
+      for (var i = 0; i < data.length; i++) {
+        var item = data[i];
+
+        if (objDef.typeKey) {
+          if (item == null) {
+            this._addError(errors, this._appendPath(dataPath, i), 'nullitem');
+          } else if ((typeof item) != 'object' || item instanceof Array) {
+            this._addError(errors, this._appendPath(dataPath, i), 'type:obj');
+          } else if (!(objDef.typeKey in item)) {
+            this._addError(errors, this._appendPath(dataPath, i), 'missingtype');
+          } else {
+            var arrayItemType = item[objDef.typeKey];
+            if (!(arrayItemType in objDef.types)) {
+              this._addError(errors, this._appendPath(dataPath, i), 'badtype');
+            } else {
+              var itemType = objDef.types[arrayItemType];
+              this._checkNode(errors, this._appendPath(dataPath, i), item, itemType);
+            }
           }
         }
       }
--- html/test/spec/utils/services/error-checker.js
+++ html/test/spec/utils/services/error-checker.js
@@ -1,8 +1,7 @@
 'use strict';
 
 describe("Service: ErrorChecker", function () {
-  var ErrorCheckerService,
-    errorChecker;
+  var ErrorCheckerService;
 
   // load the controller's module
   beforeEach(module('utils'));
@@ -12,6 +11,7 @@ describe("Service: ErrorChecker", function () {
   }));
 
   describe('Empty definition', function () {
+    var errorChecker;
 
     // Initialize the controller and a mock scope
     beforeEach(function () {
@@ -27,7 +27,7 @@ describe("Service: ErrorChecker", function () {
     it('should detect non obj root', function () {
       var dataItems = [null, 4, [1, 2], "e"];
       var result = {
-        ROOT: ['type:obj']
+        root: ['type:obj']
       };
 
       for (var i = 0; i < dataItems.length; i++) {
@@ -38,32 +38,158 @@ describe("Service: ErrorChecker", function () {
     });
   });
 
+
   describe('ROOT obj definition', function () {
+    var errorChecker;
 
     // Initialize the controller and a mock scope
     beforeEach(function () {
       errorChecker = new ErrorCheckerService({
         '_ROOT_': ['obj', {
-          'a': [true, 'n'],
-          'b': [true, 'n'],
-          'c': [false, 'n']
+          'a': [true, 'N'],
+          'b': [true, 'N'],
+          'c': [false, 'N']
         }],
-        'n': ['number']
+        'N': ['number']
       });
     });
 
     it('should detect missing obj children', function () {
       expect(errorChecker.check({})).toEqual({
-        'ROOT.a': ['missing'],
-        'ROOT.b': ['missing']
+        'root.a': ['missing'],
+        'root.b': ['missing']
       });
 
       expect(errorChecker.check({a: 1})).toEqual({
-        'ROOT.b': ['missing']
+        'root.b': ['missing']
       });
 
       expect(errorChecker.check({a: 1, b: 2})).toEqual({});
     });
+
+    it('should detect children not allowed', function () {
+      expect(errorChecker.check({a: 1, b: 2, d: null})).toEqual({
+        'root.d': ['notallowed']
+      });
+    });
+
   });
 
+
+  describe('number/string checking', function () {
+    var errorChecker;
+
+    beforeEach(function () {
+      errorChecker = new ErrorCheckerService({
+        '_ROOT_': ['obj', {
+          'a': [false, 'N'],
+          'b': [false, 'S']
+        }],
+        'N': ['number'],
+        'S': ['string']
+      });
+    });
+
+    it('should detect null values', function () {
+      expect(errorChecker.check({a: null})).toEqual({
+        'root.a': ['type:number']
+      });
+    });
+
+    it('should detect bad strings', function () {
+      expect(errorChecker.check({b: 4})).toEqual({
+        'root.b': ['type:string']
+      });
+    });
+
+    it('should pass 0 numbers and empty strings', function () {
+      expect(errorChecker.check({a: 0, b: ''})).toEqual({});
+    });
+  });
+
+  describe('array checking', function () {
+    var errorChecker;
+
+    beforeEach(function () {
+      errorChecker = new ErrorCheckerService({
+        '_ROOT_': ['obj', {
+          'a': [false, 'A']
+        }],
+        'A': ['array', {
+          typeKey: 'type',
+          types: {'c_obj': 'C_OBJ'}
+        }],
+        'STR': ['string'],
+        'NUM': ['number'],
+        'C_OBJ': ['obj', {
+          'cs': [false, 'STR'],
+          'cn': [false, 'NUM'],
+          'type': [false, 'STR']
+        }]
+      });
+    });
+
+    it('should detect array property with bad type', function() {
+      expect(errorChecker.check({a: 0})).toEqual({
+        'root.a': ['type:array']
+      });
+
+      expect(errorChecker.check({a: null})).toEqual({
+        'root.a': ['type:array']
+      });
+
+      expect(errorChecker.check({a: []})).toEqual({});
+    });
+
+    it('should detect object array null elements', function() {
+      expect(errorChecker.check({
+        a:[null]
+      })).toEqual({
+        'root.a.0': ['nullitem']
+      });
+    });
+
+    it('should detect object array non-object elements', function() {
+      expect(errorChecker.check({
+        a:[0]
+      })).toEqual({
+        'root.a.0': ['type:obj']
+      });
+    });
+
+    it('should detect object array items without type', function() {
+      expect(errorChecker.check({
+        a:[{}]
+      })).toEqual({
+        'root.a.0': ['missingtype']
+      });
+    });
+
+    it('should detect object array items with bad type', function() {
+      expect(errorChecker.check({
+        a:[{type: 'k'}]
+      })).toEqual({
+        'root.a.0': ['badtype']
+      });
+    });
+
+    it('should pass good object array', function() {
+      expect(errorChecker.check({
+        a:[{type: 'c_obj'}]
+      })).toEqual({});
+    });
+
+    it('should check object array items', function() {
+      expect(errorChecker.check({
+        a:[
+          {type: 'c_obj'},
+          {type: 'c_obj', 'cs': '', 'cn': 0},
+          {type: 'c_obj', 'cs': 0, 'cn': ''}
+        ]
+      })).toEqual({
+        'root.a.2.cs': ['type:string'],
+        'root.a.2.cn': ['type:number']
+      });
+    });
+  });
 });
